a02eb36a6ef1e341aebe599fea76a87d94d58df7
trident-kanda <tridentkandayuki@gmail.com>
2021-05-25 18:45:11 +0900

    env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import Head from "next/head";
 
 export default function Home() {
-  return <div></div>;
+  console.log(process.env.TEST);
+  return (
+    <div>
+      <h1>{process.env.TEST}</h1>
+    </div>
+  );
 }
